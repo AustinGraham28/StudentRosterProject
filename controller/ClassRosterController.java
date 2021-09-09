b3ba494d2c376a2c1ac93a7988b68846cb39912f
AustinGraham28 <89654150+AustinGraham28@users.noreply.github.com>
2021-09-08 23:46:25 -0400

    Add files via upload

--- controller/ClassRosterController.java
+++ controller/ClassRosterController.java
@@ -6,6 +6,7 @@
 package com.sg.classroster.controller;
 
 import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterDaoException;
 import com.sg.classroster.dao.ClassRosterDaoFileImpl;
 import com.sg.classroster.dto.Student;
 import com.sg.classroster.ui.ClassRosterView;
@@ -19,66 +20,77 @@ import java.util.List;
  */
 public class ClassRosterController {
     
-    private ClassRosterView view = new ClassRosterView();
-    private ClassRosterDao dao = new ClassRosterDaoFileImpl();
+    //private ClassRosterView view = new ClassRosterView();
+    //private ClassRosterDao dao = new ClassRosterDaoFileImpl();
+    private ClassRosterView view;
+    private ClassRosterDao dao;
     private UserIO io = new UserIOConsoleImpl();
+    
+    public ClassRosterController(ClassRosterDao daoIn, ClassRosterView viewIn){
+        this.dao = daoIn;
+        this.view = viewIn;
+    }
     
     public void run(){
         boolean keepGoing = true;
         int menuSelection = 0;
         
-        while(keepGoing){
-            
-            menuSelection = getMenuSelection();
-            
-            switch(menuSelection){
-                case 1: 
-                    listStudents();
-                    break;
-                case 2:
-                    createStudent();
-                    break;
-                case 3:
-                    viewStudent();
-                    break;
-                case 4:
-                    removeStudent();
-                    break;
-                case 5:
-                    keepGoing = false;
-                    break;
-                default:
-                    unknownCommand();
+        try{
+            while(keepGoing){
+
+                menuSelection = getMenuSelection();
+
+                switch(menuSelection){
+                    case 1: 
+                        listStudents();
+                        break;
+                    case 2:
+                        createStudent();
+                        break;
+                    case 3:
+                        viewStudent();
+                        break;
+                    case 4:
+                        removeStudent();
+                        break;
+                    case 5:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
             }
+            exitMessage();
+        }catch(ClassRosterDaoException e){
+            view.displayErrorMessage(e.getMessage());
         }
-        exitMessage();
     }
     
     private int getMenuSelection(){
         return view.printMenuAndGetSelection();
     }
     
-    private void createStudent(){
+    private void createStudent() throws ClassRosterDaoException {
         view.displayCreateStudentBanner();
         Student newStudent = view.getNewStudentInfo();
         dao.addStudent(newStudent.getStudentId(), newStudent);
         view.displaySuccessBanner();
     }
     
-    private void listStudents(){
+    private void listStudents() throws ClassRosterDaoException {
         view.displayDisplayAllBanner();
         List<Student> studentList = dao.getAllStudents();
         view.displayStudentList(studentList);
     }
     
-    private void viewStudent(){
+    private void viewStudent() throws ClassRosterDaoException {
         view.displayDisplayAllBanner();
         String studentId = view.getStudentIdChoice();
         Student student = dao.getStudent(studentId);
         view.displayStudent(student);
     }
     
-    private void removeStudent(){
+    private void removeStudent() throws ClassRosterDaoException {
         view.displayRemoveStudentBanner();
         String studentId = view.getStudentIdChoice();
         Student removedStudent = dao.removeStudent(studentId);
